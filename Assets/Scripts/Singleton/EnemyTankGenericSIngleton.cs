97a4e6ec466d9f4928a2f9781aea3e81f8a7bd82
HoneyJain11 <hrjain911@gmail.com>
2021-12-03 21:59:55 +0530

    Added generic singleton for PlayerTank as well as EnemyTank.

---
+++ Assets/Scripts/Singleton/EnemyTankGenericSIngleton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTankGenericSingleton<T> : MonoBehaviour where T : EnemyTankGenericSingleton<T>
+{
+    private static T instance;
+    public static T Instances { get { return instance; } }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = (T)this;
+        }
+        else
+        {
+            Debug.LogError("Creating a duplicate singleton");
+            Destroy(this);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Singleton/PlayerTankGenericSingleton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTankGenericSingleton<T> : MonoBehaviour where T : PlayerTankGenericSingleton<T>
+{
+    private static T instance;
+    public static T Instance { get { return instance; } }
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = (T)this;
+        }
+        else
+        {
+            Debug.LogError("Creating a duplicate singleton");
+            Destroy(this);
+        }
+        
+    }
+
+}
